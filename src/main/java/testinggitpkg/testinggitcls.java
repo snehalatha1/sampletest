c0599ede17fdd23a336ae4ce9fe2f6e3e05fdaa8
snehalatha1 <snehalathaps15@gmail.com>
2022-12-21 15:45:43 +0530

    first commit

---
+++ src/main/java/testinggitpkg/testinggitcls.java
@@ -0,0 +1,11 @@
+package testinggitpkg;
+
+public class testinggitcls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated mthod stub
+		System.out.println("trying to execute the programs");
+
+	}
+
+}
